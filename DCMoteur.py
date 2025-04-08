b73f3f5cacd79e9ab9798d68998befe1a1ae58c9
JalJim <adambougma55@gmail.com>
2025-04-08 09:23:34 +0200

    Translation
    from french to english

--- DCMoteur.py
+++ DCMoteur.py
@@ -3,15 +3,15 @@ import time
 import PCA9685 as PCA
 
 
-class DCMoteur:
+class DCMotor:
     def __init__(self):
-        self.__MotorG_A = 17 
-        self.__MotorG_B = 18 
-        self.__MotorD_A = 27 
-        self.__MotorD_B = 22 
+        self.__MotorL_A = 17 
+        self.__MotorL_B = 18 
+        self.__MotorR_A = 27 
+        self.__MotorR_B = 22 
         self.__EN_MG = 4 
         self.__EN_MD = 5 
-        self.__pins = [self.__MotorG_A, self.__MotorG_B, self.__MotorD_A, self.__MotorD_B] 
+        self.__pins = [self.__MotorL_A, self.__MotorL_B, self.__MotorR_A, self.__MotorR_B] 
         
         self.__pwm = PCA.PWM() 
         self.__pwm.frequency = 60 
@@ -22,32 +22,32 @@ class DCMoteur:
         for pin in self.__pins: 
             GPIO.setup(pin, GPIO.OUT)   
 
-    def __set_etat_moteur(self, motor_a, motor_b, pwm_value): 
+    def __set_motor_state(self, motor_a, motor_b, pwm_value): 
         GPIO.output(motor_a, GPIO.HIGH if pwm_value > 0 else GPIO.LOW) 
         GPIO.output(motor_b, GPIO.LOW if pwm_value > 0 else GPIO.HIGH) 
-        self.__pwm.write(self.__EN_MG if motor_a == self.__MotorG_A else self.__EN_MD, 0, int(abs(pwm_value))) 
+        self.__pwm.write(self.__EN_MG if motor_a == self.__MotorL_A else self.__EN_MD, 0, int(abs(pwm_value))) 
 
-    def moteur_avance(self,vitesse = 100): 
-        self.__set_etat_moteur(self.__MotorG_A, self.__MotorG_B, self.__convertir_vitesse(vitesse))
-        self.__set_etat_moteur(self.__MotorD_A, self.__MotorD_B, self.__convertir_vitesse(vitesse))
+    def motor_forward(self,vitesse = 100): 
+        self.__set_motor_state(self.__MotorL_A, self.__MotorL_B, self.__convert_speed(vitesse))
+        self.__set_motor_state(self.__MotorR_A, self.__MotorR_B, self.__convert_speed(vitesse))
 
-    def moteur_recule(self,vitesse = -100):
+    def motor_backward(self,vitesse = -100):
         if ((vitesse)<0):
-            self.__set_etat_moteur(self.__MotorG_A, self.__MotorG_B, self.__convertir_vitesse(vitesse))
-            self.__set_etat_moteur(self.__MotorD_A, self.__MotorD_B, self.__convertir_vitesse(vitesse))
+            self.__set_motor_state(self.__MotorL_A, self.__MotorL_B, self.__convert_speed(vitesse))
+            self.__set_motor_state(self.__MotorR_A, self.__MotorR_B, self.__convert_speed(vitesse))
         else:
             raise ValueError("La vitesse doit etre négative !")
 
 
-    def stop_moteur(self):
-        self.__set_etat_moteur(self.__MotorG_A, self.__MotorG_B, 0)
-        self.__set_etat_moteur(self.__MotorD_A, self.__MotorD_B, 0) 
+    def stop_motor(self):
+        self.__set_motor_state(self.__MotorL_A, self.__MotorL_B, 0)
+        self.__set_motor_state(self.__MotorR_A, self.__MotorR_B, 0) 
 
-    def __convertir_vitesse(self, vitesse): 
+    def __convert_speed(self, vitesse): 
         vitesse = max(-100, min(100, vitesse))
         return vitesse * 4095 / 100
     
-    def stop_urgence(self):
+    def emergency_stop(self):
         print("Arrêt d'urgence des moteurs !")
         self.stop_moteur()
         
