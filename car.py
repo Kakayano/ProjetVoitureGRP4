10b0b6d215063c1addff602e87eb826d7a824adb
Kakayano <neo.herbigniaux@std.heh.be>
2025-04-08 11:42:45 +0200

    feat: ajout d'un input demandant le nombre de tour

--- car.py
+++ car.py
@@ -1,7 +1,7 @@
 from sensors.rgb_sensor import RGBSensor
 from sensors.ultrasonic_sensor import UltrasonicSensor
 from sensors.CurrentSensor import CurrentSensor
-from sensors.LineFollowSensor import LineFollowerSensor
+from sensors.LineFollowSensor import LineFollowSensor
 from motors.dc_motor import DCMotor
 from motors.servo_motor import ServoMotor
 
@@ -12,7 +12,7 @@ class Car():
         self.__right_ultrasonic_sensor = UltrasonicSensor("Ultrasonic", "GPIO", 26, 19)
         self.__left_ultrasonic_sensor = UltrasonicSensor("Ultrasonic", "GPIO", 11, 9)
         self.__current_sensor = CurrentSensor("Current", "I2C")
-        self.__line_sensor = LineFollowerSensor("LineFollower", "GPIO")
+        self.__line_sensor = LineFollowSensor("LineFollower", "GPIO")
         self.__dc_motor = DCMotor()
         self.__servo_motor = ServoMotor()
         self.__limit = 10
@@ -24,6 +24,7 @@ class Car():
         """
         Démarre la voiture et lit les données des capteurs.
         """
+        number_of_laps = int(input("Enter the number of laps: "))
         while True:
             
             ina_data = self.__current_sensor.read_data()
@@ -44,19 +45,25 @@ class Car():
             
             if front_distance < 30:
                 angle = 15
+                self.__dc_motor.moteur_avance(75)
             elif front_distance < 20:
                 angle = 30
+                self.__dc_motor.moteur_avance(50)
             elif front_distance < self.__limit:
                 angle = 45
                 self.__dc_motor.stop_moteur()
             else: 
                 angle = 0
+                self.__dc_motor.moteur_avance()
                 
             self.__servo_motor.set_angle(direction * angle)
             
             if self.__line_sensor.is_on_line():
                 self.__laps += 1
                 print(f"Number of laps: {self.__laps}")
+                if self.__laps >= number_of_laps:
+                    self.__dc_motor.stop_moteur()
+                    break
                 
                 
                 
